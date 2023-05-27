2e46bfffc422fb5b5f1ad3364371ce2dc7952efa
nabilfikrisp <mnabilfikrisp@gmail.com>
2023-05-27 12:45:45 +0700

    coba flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,34 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                serif: ["Merriweather", "serif"],
+            },
+            borderRadius: {
+                "4xl": "2rem",
+            },
+            colors: {
+                primary: "#F4F6F8",
+                secondary: "#333333",
+                accent3: "#3772FF",
+                accent2: "#FF8800",
+                accent1: "#551283",
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("flowbite/plugin"), require("@tailwindcss/forms")],
 };
