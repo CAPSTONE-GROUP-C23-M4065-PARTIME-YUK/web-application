0fe17314d0315000dc40c935dc708e346da64ae7
DANNN-GIT <e41201452@student.polije.ac.id>
2023-05-28 17:57:18 +0700

    Membuat seeder untuk tabel users

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Administrator',
+                'email' =>  'administrator@gmail.com',
+                'password' => Hash::make('loginadministrator'),
+                'role' => 'administrator',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Employer',
+                'email' =>  'employer@gmail.com',
+                'password' => Hash::make('loginemployer'),
+                'role' => 'employer',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Job Seeker',
+                'email' =>  'jobseeker@gmail.com',
+                'password' => Hash::make('loginjobseeker'),
+                'role' => 'jobseeker',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ]);
+    }
+}
